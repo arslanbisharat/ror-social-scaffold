689a8420d3b935782c738dc0fcca8e8e0d4a4538
Muhammad Arslan <thearslan380@gmail.com>
2020-06-23 14:42:54 -0700

    Update tests for post model and add tests for like model

--- spec/factories/models/post_spec.rb
+++ spec/factories/models/post_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-let(:post) {Post.create(content: "john is great programmer")}
+  let(:post) {Post.create(content: "john is great programmer")}
 
   describe 'validations' do
     it { should validate_presence_of(:content) }
-    it { should validate_length_of(:content).is_at_most(1000) }
+    it { should validate_length_of(:content).is_at_most(1000).with_message('1000 characters in post is the maximum allowed.') }
+   
   end
 
   describe 'associations' do
