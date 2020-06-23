2e2ead52f9723142026b80e061bada08252e487a
Muhammad Arslan <thearslan380@gmail.com>
2020-06-23 15:02:13 -0700

    Add itegration tests for comment and like models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 let(:user) {User.create(name: "john", email: "johndoe@example.com")}
-
+let(:user1) do
+  User.create(name: 'Foobar', email: 'foobar@gmail.com',
+              password: 'foobar', password_confirmation: 'foobar')
+end
+let(:friend) do
+  User.create(name: 'Footbar', email: 'footbar@gmail.com',
+              password: 'foobar', password_confirmation: 'foobar')
+end
+let(:friend_request) { FriendRequest.create(user_id: user1.id, friend_id: friend.id, confirmed: false) }
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_length_of(:name).is_at_most(20) }
@@ -19,4 +27,11 @@ let(:user) {User.create(name: "john", email: "johndoe@example.com")}
     it { should have_many(:friend_requests)}
     it { should have_many(:inverse_friend_requests).class_name("FriendRequest").with_foreign_key(:friend_id) }
   end
+
+  describe 'validates friendship associations' do
+    it 'validates if friend is friends' do
+      expect(user1.friends).to_not include(friend)
+    end
+  end
+
 end
