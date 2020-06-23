bff2e857fe018554ccc5c4255a163a620e705728
Muhammad Arslan <thearslan380@gmail.com>
2020-06-23 14:47:01 -0700

    Update factories folder

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
     factory :user do
       name { 'Sample' }
-      email { 'example@gmail.com' }
-    end
+      email { 'sample@gmail.com' }
+      password { '12345678' }
+      password_confirmation { '12345678' }    end
 end 
